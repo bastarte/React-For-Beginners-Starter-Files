6838d1897e0b112357efec5e622d8f1496e25a46
Bastien Hubervic-Renard <bastienhr+github@gmail.com>
2021-05-17 16:19:19 +0200

    basic homepage and storeApp + routing

--- catch-of-the-day/src/index.js
+++ catch-of-the-day/src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { render } from 'react-dom';
 // import { StorePicker } from './components/StorePicker' // if export { StorePicker };
-import StorePicker from './components/StorePicker'
+import Router from "./components/Router";
+import "./css/style.css";
 
-render(<StorePicker/>, document.getElementById('main'));
+render(<Router />, document.getElementById('main'));
