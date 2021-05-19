bffa33195a9f86b7fab9cee1eb4fc11726dad58e
Bastien Hubervic-Renard <bastienhr+github@gmail.com>
2021-05-19 12:28:04 +0200

    Load and dispay sample fish

---
+++ catch-of-the-day/src/components/Fish.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { formatPrice } from "../helpers";
+
+class Fish extends React.Component {
+  render() {
+    const {name, image, status, price, desc } = this.props.details ;
+    return (
+      <li className="menu-fish">
+        <img src={image} alt={name} />
+        <h3 className="fish-name">
+          {name}
+          <span className="price">{formatPrice(price)}</span>
+        </h3>
+        <span>{status}</span>
+        <p>{desc}</p>
+        <button>Add to cart</button>
+      </li>
+    );
+  }
+}
+
+export default Fish;
